410c2fcc559a00255ca20ad70a4dab3fd9a53f79
Arnaud Berthomier <oz@cyprio.net>
2011-06-18 17:14:26 +0200

    Hack to load an external audio file and play it
     * Remove sample Sencha application cruft
     * Include zepto.js
     * Move JS in js/index.js

--- public/js/index.js
+++ public/js/index.js
@@ -5,35 +5,17 @@ Ext.setup({
     phoneStartupScreen: 'phone_startup.png',
     glossOnIcon: false,
     onReady: function(){
-        // Create a new draggable for the div with an
-        // id of 'draggable'
-        new Ext.util.Draggable('draggable', {
-            revert: true
-        });
 
-        // Create a new draggable for the div with an
-        // id of 'invalid
-        new Ext.util.Draggable('invalid', {
-            // Specify a group that won't have a valid
-            // Droppable target
-            group: 'invalid',
-            // Revert the Draggable back to its original
-            // position on an invalid drop.
-            revert: true
-        });
+      var player = $("#player")[0];
 
-        // Create a new Droppable for the div with an
-        // id of 'droppable'
-        new Ext.util.Droppable('droppable', {
-            // Change the validDropMode from the default of 'intersect' to
-            // 'contains' this ensures that a Draggable must be completed
-            // contained by the Droppable in order to perform a drop
-            validDropMode: 'contains',
-            listeners: {
-                drop: function(droppable, draggable, e) {
-                    draggable.el.setHTML('Dropped!');
-                }
-            }
-        });
+      $("#play").bind('click', function(event) {
+        console.log( $("#url").val() );
+
+        event.preventDefault();
+        $( player ).attr('src', $("#url").val());
+        player.play();
+      });
+
     }
 });
+

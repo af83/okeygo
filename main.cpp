5f2b3d17f1f4f6c4f5a22bf8eaacce029801da5b
Arnaud Berthomier <oz@cyprio.net>
2011-06-18 21:48:26 +0200

    Allow loading of remote content with XHR

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <QtGui/QApplication>
 #include <QtWebKit/QWebView>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("Web");
-
     QWebView view;
+    view.settings()->setAttribute(QWebSettings::LocalContentCanAccessRemoteUrls, true);
     view.load(QUrl("qrc:/public/index.html"));
     view.show();
-
     return a.exec();
 }

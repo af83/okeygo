8db2e46472c443b917a6c3ab23848dcd61ea979f
Laurent Arnoud <laurent@spkdev.net>
2011-06-19 02:46:23 +0200

    Try to use the list
    Signed-off-by: Laurent Arnoud <laurent@spkdev.net>

--- public/js/index.js
+++ public/js/index.js
@@ -153,62 +153,20 @@ Ext.setup({
       // Build a sortable song list show on home
       Ext.regModel('Songs', { fields: ['artist', 'name'] });
       var store = new Ext.data.JsonStore({
+        autoload: true,
+        url: 'js/songs.json',
+        root: 'data',
+        fields: ['artist', 'songs'],
         model  : 'Songs',
         sorters: 'artist',
         getGroupString : function(record) {
           return record.get('artist')[0];
         },
         data: [
-          {artist: "Europe",
-           song:   "The final countdown",
-           lyrics: "songs/Europe_-_The_final_countdown/Europe_-_The_Final_Countdown.txt",
-           url:    "songs/Europe_-_The_final_countdown/Europe_-_The_Final_Countdown.mp3",
-          },
-          {artist: "Metallica",
-           song:   "Nothing else matters",
-           lyrics: "songs/Metallica_-_Nothing_Else_Matters/Metallica_-_Nothing_Else_Matters.txt",
-           url:    "songs/Metallica_-_Nothing_Else_Matters/Metallica_-_Nothing_Else_Matters.mp3",
-          },
-          {artist: "Metallica",
-           song:   "The Unforgiven",
-           lyrics: "songs/Metallica_-_The_Unforgiven_I/Metallica_-_The_Unforgiven_I.txt",
-           url:    "songs/Metallica_-_The_Unforgiven_I/Metallica_-_The_Unforgiven_I.mp3",
-          },
-          {artist: "Opus",
-           song:   "Life is Life",
-           lyrics: "songs/Opus_-_Life_is_Life/Opus_-_Live_Is_Life.txt",
-           url:    "songs/Opus_-_Life_is_Life/Opus_-_Live_Is_Life.mp3",
-          },
-          {artist: "Pink Floyd",
-           song:   "Another Brick In The Wall",
-           lyrics: "songs/Pink_Floid_-_Another_Brick_In_The_Wall/Pink_Floid_-_Another_Brick_In_The_Wall.txt",
-           url:    "songs/Pink_Floid_-_Another_Brick_In_The_Wall/Pink_Floid_-_Another_Brick_In_The_Wall.mp3",
-          },
-          {artist: "Rednex",
-           song:   "Cotton Eye Joe",
-           lyrics: "songs/Rednex_-_Cotton_Eye_Joe/Rednex_-_Cotton_Eye_Joe.txt",
-           url:    "songs/Rednex_-_Cotton_Eye_Joe/Rednex_-_Cotton_Eye_Joe.mp3",
-          },
-          {artist: "Hare Hare Yukai",
-           song:   "The Melancholy of Harui Syzumiya",
-           lyrics: "songs/The_Melancholy_of_Haruhi_Suzumiya_-_Hare_Hare_Yukai/The_Melancholy_of_Haruhi_Suzumiya_-_Hare_Hare_Yukai.txt",
-           url:    "songs/The_Melancholy_of_Haruhi_Suzumiya_-_Hare_Hare_Yukai/The_Melancholy_of_Haruhi_Suzumiya_-_Hare_Hare_Yukai.mp3",
-          },
-          {artist: "The Village people",
-           song:   "YMCA",
-           lyrics: "songs/The_Village_People_-_YMCA/The_Village_People_-_YMCA.txt",
-           url:    "songs/The_Village_People_-_YMCA/The_Village_People_-_YMCA.mp3",
-          },
-          {artist: "Celine Dion",
-           song:   "My heart will go on",
-           lyrics: "songs/Celine_Dion_-_My_Heart_Will_Go_On/Celine_Dion_-_My_Heart_Will_Go_On.txt",
-           url:    "songs/Celine_Dion_-_My_Heart_Will_Go_On/Celine_Dion_-_My_Heart_Will_Go_On.mp3"
-          },
-          {artist: "Deep Purple",
-           song:   "Smoke on the water",
-           lyrics: "songs/Deep_Purple_-_Smoke_On_The_Water/Deep_Purple_-_Smoke_On_The_Water.txt",
-           url:    "songs/Deep_Purple_-_Smoke_On_The_Water/Deep_Purple_-_Smoke_On_The_Water.mp3"
-          }
+            {artist: "Zed Shaw",    song: "Matz can't patch"},
+            {artist: "Celine Dion", song: "I love MeeGo"},
+            {artist: "Deep Purple", song: "Smoke on the water"},
+            {artist: "Dr Dre",      song: "Lyrical Gang-Bang"}
         ]
       });
       // Song list Ext widget

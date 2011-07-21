514c7c9d2eb6dd2ec66f69484cd434a4d6321f09
Laurent Arnoud <laurent@spkdev.net>
2011-07-21 22:34:00 +0200

    Clean lyrics txt files
    Signed-off-by: Laurent Arnoud <laurent@spkdev.net>

--- scripts/songs_lists.sh
+++ scripts/songs_lists.sh
@@ -4,31 +4,54 @@
 file="public/js/songs.json"
 song_path='public/songs'
 
-lists=`ls $song_path`
+# space 2 underscore
+find $song_path -name "* *" | nl -ba | sort -nr | cut -f2 > $song_path/tmp.txt
+while read iline; do
+    predicate=`echo $iline| awk -F "/" '{  print $NF }' | tr " " "_"`
+    mv "$iline" "`dirname "$iline"`/$predicate"
+done < $song_path/tmp.txt
+rm $song_path/tmp.txt
+
+lists=`find $song_path -iname "*.txt" -type f`
 firstline='';
 
 echo '{"data":[' > $file
-for line in $lists; do
-	## artist extract
-	artist=$(echo $line | cut -d"-" -f1 | tr "_" " " | sed -e 's/ $//')
-	## song extract
-	song=$(echo $line | cut -d"-" -f2 | tr "_" " " | sed -e 's/^ //')
+for txt in $lists; do
+    # convert \r\n --> \n
+    if $(file "$txt" | grep CRLF) ; then
+        echo " >>> converting from CRLF to LF"
+        sed -i 's/\x0D$//' $txt
+    fi
 
-	# comma not for the first line
-	if [ -z $firstline ]; then
-		firstline='xxx'
-	else
-		echo -n ',' >> $file
-	fi
+    if $(file "$txt" | grep -q ISO); then
+        echo " >>> the files is not UTF-8"
+        vim +'set encoding=utf-8' +'set fileencoding=utf-8' +'write ++enc=utf-8' +'quit' "$txt"
+    fi
 
-	echo '{"artist":"'$artist'", "song":"'$song'"' >> $file
-	url=$(find $song_path/$line -type f -iname "*.mp3" | sed -e "s|public/||")
-	echo ', "url":"'$url'"' >> $file
+    ARTIST=`grep     "#ARTIST:"     "$txt" | cut -d : -f 2`
+    TITLE=`grep      "#TITLE:"      "$txt" | cut -d : -f 2`
+    BPM=`grep        "#BPM:"        "$txt" | cut -d : -f 2`
 
-    lyrics=$(find $song_path/$line -type f -iname "*.txt" | sed -e "s|public/||")
-	echo ', "lyrics":"'$lyrics'"' >> $file
+    if [ "x$ARTIST" = "x" -o "x$TITLE" = "x" -o "x$BPM" = "x" ] ; then
+        echo " >>> error: this does not seem to ba a valid ultrastar file."
+        exit 1
+    fi
 
-    img=$(find $song_path/$line -type f -iname "*.jpg" -exec du -sk {} \; |
+    # comma not for the first line
+    if [ -z $firstline ]; then
+        firstline='xxx'
+    else
+        echo -n ',' >> $file
+    fi
+
+    echo '{"artist":"'$ARTIST'", "song":"'$TITLE'"' >> $file
+    url=$(find `dirname $txt` -type f -iname "*.mp3" | sed -e "s|public/||")
+    echo ', "url":"'$url'"' >> $file
+
+    lyrics=$(echo $txt | sed -e "s|public/||")
+    echo ', "lyrics":"'$lyrics'"' >> $file
+
+    img=$(find `dirname $txt` -type f -iname "*.jpg" -exec du -sk {} \; |
     sort -nr | head -n1 | awk '{print $2}' | sed -e "s|public/||")
     if [ -z "$img" ]; then
         echo ', "img":"images/cover_fallback.jpg"' >> $file
